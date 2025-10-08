4c31d5d9a5d5a11ecbf05e36a16754d1b6ff6a47
dhruv <dhruv.sn2003@gmail.com>
2025-10-08 15:07:32 +0530

    add query 4

---
+++ project_sql/04_top_paying_skill.sql
@@ -0,0 +1,133 @@
+/*
+Question: What are the top skills based on salary?
+>Look at the average salary associated with each skill fro Data Analyst roles
+>Focus on roles with specified salaries regardless of location
+>Why? It reveals how different skills impact salary levels for Data Analyst and helps identify
+ the most financially rewarding skills in the field.
+*/
+
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg),0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25;
+
+/*
+>The Data Analyst/Engineer Hybrid Pays Most: The largest group of high-paying skills is in DevOps, Automation, and Infrastructure (36% of the list), including tools like Terraform, Ansible, and Airflow, indicating top analysts must also manage and automate data pipelines.
+>Machine Learning (ML) is an Analyst Requirement: Nearly a quarter of the skills are ML frameworks (Keras, PyTorch, TensorFlow, Hugging Face), showing that high-paying "Analyst" roles involve building and deploying models, blurring into Applied Scientist roles.
+>Extreme Premium on Niche/Emerging Tech: Skills in complex or emerging domains—like Solidity (Blockchain, $179k) and high-speed NoSQL/Big Data (Couchbase, Kafka)—command significantly higher salaries due to specialized knowledge and low talent supply.
+>Salaries Driven by Engineering-Grade Languages: Programming skills like Golang ($155k) and Scala are highly compensated, indicating that top analysts operate in high-performance, engineering-heavy environments.
+
+
+[
+  {
+    "skills": "svn",
+    "avg_salary": "400000"
+  },
+  {
+    "skills": "solidity",
+    "avg_salary": "179000"
+  },
+  {
+    "skills": "couchbase",
+    "avg_salary": "160515"
+  },
+  {
+    "skills": "datarobot",
+    "avg_salary": "155486"
+  },
+  {
+    "skills": "golang",
+    "avg_salary": "155000"
+  },
+  {
+    "skills": "mxnet",
+    "avg_salary": "149000"
+  },
+  {
+    "skills": "dplyr",
+    "avg_salary": "147633"
+  },
+  {
+    "skills": "vmware",
+    "avg_salary": "147500"
+  },
+  {
+    "skills": "terraform",
+    "avg_salary": "146734"
+  },
+  {
+    "skills": "twilio",
+    "avg_salary": "138500"
+  },
+  {
+    "skills": "gitlab",
+    "avg_salary": "134126"
+  },
+  {
+    "skills": "kafka",
+    "avg_salary": "129999"
+  },
+  {
+    "skills": "puppet",
+    "avg_salary": "129820"
+  },
+  {
+    "skills": "keras",
+    "avg_salary": "127013"
+  },
+  {
+    "skills": "pytorch",
+    "avg_salary": "125226"
+  },
+  {
+    "skills": "perl",
+    "avg_salary": "124686"
+  },
+  {
+    "skills": "ansible",
+    "avg_salary": "124370"
+  },
+  {
+    "skills": "hugging face",
+    "avg_salary": "123950"
+  },
+  {
+    "skills": "tensorflow",
+    "avg_salary": "120647"
+  },
+  {
+    "skills": "cassandra",
+    "avg_salary": "118407"
+  },
+  {
+    "skills": "notion",
+    "avg_salary": "118092"
+  },
+  {
+    "skills": "atlassian",
+    "avg_salary": "117966"
+  },
+  {
+    "skills": "bitbucket",
+    "avg_salary": "116712"
+  },
+  {
+    "skills": "airflow",
+    "avg_salary": "116387"
+  },
+  {
+    "skills": "scala",
+    "avg_salary": "115480"
+  }
+]
+*/
